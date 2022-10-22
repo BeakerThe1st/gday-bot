6f1e2b8a4f63904d9b7e6c410823865ce722bd4d
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-10-23 07:26:42 +1100

    Update emoteBan.ts

--- src/plugins/emoteBan.ts
+++ src/plugins/emoteBan.ts
@@ -2,12 +2,11 @@ import { Message } from "discord.js";
 import { useEvent } from "../hooks";
 
 useEvent("messageCreate", async (message: Message) => {
-  if (message.content.includes("🤭")) {
-    if (message.author.id === "863834256484728862") {
-      await message.delete();
-      await message.channel.send(
-        `Sorry ${message.author} you are banned from using that emote. 🤭🤭🤭`
-      );
-    }
+  const { author } = message;
+  if (author.id === "863834256484728862") {
+    await message.delete();
+    await message.channel.send(
+      `Sorry ${author} you are banned from sending messages.`
+    );
   }
 });
