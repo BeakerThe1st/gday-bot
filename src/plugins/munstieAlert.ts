ee05f7afc8550209a177ddc7042cea1390d72ffa
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-11-20 05:37:40 +1100

    Create munstieAlert.ts

---
+++ src/plugins/munstieAlert.ts
@@ -0,0 +1,16 @@
+import { Message } from "discord.js";
+import { useEvent } from "../hooks";
+
+useEvent("messageCreate", async (message: Message) => {
+  if (!message.guild) {
+    return;
+  }
+  if (
+    message.guild.id === "332309672486895637" &&
+    message.content.toLowerCase().includes("munstie")
+  ) {
+    if (!message.mentions.users.get("247133649206640640")) {
+      await message.reply("<@247133649206640640> 🤪");
+    }
+  }
+});
