9552bf634aec667799f32faecbaa9aa9e4110801
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-05-24 16:46:03 +1000

    Rearrange hook exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 //EXCLUDE useChatCommand - it causes some weird stuff with ESM
-export * from "./useClient";
 export * from "./useEnv";
+export * from "./useClient";
 export * from "./useError";
 export * from "./useEvent";
