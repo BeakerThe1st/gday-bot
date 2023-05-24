2f94830d83fc2ced401be7ba0a35b9c755d7b71a
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-05-24 16:46:34 +1000

    Create useOpenAI hook

---
+++ src/hooks/useOpenAI.ts
@@ -0,0 +1,12 @@
+import {useEnv} from "./useEnv";
+import {Configuration, OpenAIApi} from "openai";
+
+const configuration = new Configuration({
+    apiKey: useEnv("OPENAI_KEY")
+})
+
+const openai = new OpenAIApi(configuration);
+
+export const useOpenAI = () => {
+    return openai;
+}
