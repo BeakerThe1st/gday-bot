1de5884b220d9ff1fc764e9ac28d14af33f7f686
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-05-26 23:34:30 +1000

    Change to chat gpt model

--- src/commands/ask.ts
+++ src/commands/ask.ts
@@ -12,10 +12,13 @@ const builder = new SlashCommandBuilder()
 
 useChatCommand(builder as SlashCommandBuilder, async (interaction: ChatInputCommandInteraction) => {
     const prompt = interaction.options.getString("prompt", true);
-    const completion = await useOpenAI().createCompletion({
-        model: "text-davinci-003",
-        prompt: `${prompt}. Answer in the style of an Australian using Australian words and slang.`,
-        max_tokens: 512
+    const completion = await useOpenAI().createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {role: "system", content: "Your name is G'day. You are a Discord bot created by Beaker. You are currently in the r/Apple Discord server. You are themed as an Australian person, and you therefore must answer all prompts with Australian slang and in the style of an Australian."},
+            {role: "user", content: prompt}
+        ],
+        max_tokens: 256,
     });
-    return completion.data.choices[0].text ?? "Unable to do that";
+    return completion.data.choices[0].message ?? "Unable to do that";
 });
