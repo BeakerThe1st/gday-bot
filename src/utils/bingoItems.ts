09155f5d9ef0eb944267f8b1f197d84b2fc4deb8
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-06-05 14:59:07 +1000

    Readd macos malibu

--- src/utils/bingoItems.ts
+++ src/utils/bingoItems.ts
@@ -2,7 +2,7 @@ export const bingoItems = new Map<string, boolean>()
 
 const items = ["15_inch_mba", "ar", "ar_devkit", "ar_os", "camera_app", "carplay",
     "good_mornang", "icloud_storage_increase", "imac", "ios_compass", "ios_lock_screen",
-    "ipad_calculator", "ipad_xcode", "iphone_os", "mac_pro", "mac_studio", "macos_sequoia", "m2_ultra",
+    "ipad_calculator", "ipad_xcode", "iphone_os", "mac_pro", "mac_studio", "macos_malibu", "macos_sequoia", "m2_ultra",
     "memerighi", "one_more_thing", "vr_facetime", "vr_iphone_apps", "watch_widgets", "free_space"];
 
 
