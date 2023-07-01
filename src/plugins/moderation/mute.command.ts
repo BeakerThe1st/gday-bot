5cbaea9d0fe65e1f579274b52457d63b5e2b06c2
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-07-01 20:44:07 +1000

    Reformat code and make changes

--- src/plugins/moderation/mute.command.ts
+++ src/plugins/moderation/mute.command.ts
@@ -1,4 +1,12 @@
 import { ChatInputCommandInteraction, PermissionFlagsBits, time, userMention } from "discord.js";
+import {
+    ChatInputCommandInteraction,
+    inlineCode,
+    PermissionFlagsBits,
+    time,
+    TimestampStyles,
+    userMention
+} from "discord.js";
 import {SlashCommandBuilder, SlashCommandScope} from "../../builders/SlashCommandBuilder";
 import {useChatCommand} from "../../hooks/useChatCommand";
 import parse from 'parse-duration';
@@ -6,36 +14,36 @@ import parse from 'parse-duration';
 const builder = new SlashCommandBuilder()
     .setName("mute")
     .setDescription("Mutes user for the specified duration.")
-    .addUserOption((option) => 
+    .addUserOption((option) =>
         option.setName("user").setDescription("User to be muted.").setRequired(true)
     )
     .addStringOption((option) =>
-        option.setName("duration").setDescription("Duration (max 28d, default 10m)").setRequired(false)
+        option.setName("duration").setDescription("Duration (max 28d, default 10m)").setRequired(true)
     )
     .addStringOption((option) =>
-    option.setName("reason").setDescription("Reason for the mute.").setRequired(false)
+        option.setName("reason").setDescription("Reason for the mute.")
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
     .setScope(SlashCommandScope.GLOBAL);
 
 useChatCommand(builder, async (interaction: ChatInputCommandInteraction) => {
-        const MAX_MUTE = 2419200000 // 28 days in ms, limit provided by Discord API.
-        const user = interaction.options.getUser("user", true);
-        const duration = interaction.options.getString("duration") ?? "10m";
-        const reason = interaction.options.getString("reason") ?? "No reason provided.";
-        const parsedDuration = parse(duration, 'ms') ?? 600000; // 10 minutes to ms to get around parse returning undefined potentially.
+    const MAX_MUTE = 2419200000 // 28 days in ms, limit provided by Discord API.
 
-        if (!interaction.guild) return `This command can only be used in a guild.` 
-        if (parsedDuration > MAX_MUTE) return `You cannot mute anyone for more than 28 days.`; 
-        const member = await interaction.guild.members.fetch(user);
-        if (!member) return `This member is not a member of this guild.`
-        try {
-            const lastMutedTimestamp = member.communicationDisabledUntilTimestamp;
-            if (lastMutedTimestamp && lastMutedTimestamp > Date.now()) return `${userMention(member.id)} is already muted!`
-            await member.timeout(parsedDuration, reason);
-            const timestamp = Math.floor(Date.now()/1000 + parsedDuration/1000);
-            return `${userMention(member.id)} has been muted for ${duration}. (${reason})\nExpiring <t:${timestamp}:R>.`
-        } catch (e) {
-            return `Something went wrong with the mute command: ${e}`
-        }
+    const user = interaction.options.getUser("user", true);
+    const duration = interaction.options.getString("duration", true);
+    const reason = interaction.options.getString("reason") ?? undefined; // ?? undefined as getString() returns string | null
+    const parsedDuration = parse(duration, 'ms') ?? 600000; // 10 minutes to ms to get around parse returning undefined potentially.
+
+    if (!interaction.guild) return `This command can only be used in a guild.`
+    if (parsedDuration > MAX_MUTE) return `You cannot mute anyone for more than 28 days.`;
+
+    const member = await interaction.guild.members.fetch(user);
+
+    /*const lastMutedTimestamp = member.communicationDisabledUntilTimestamp;
+
+    if (lastMutedTimestamp && lastMutedTimestamp > Date.now()) return `${userMention(member.id)} is already muted!`*/
+
+    await member.timeout(parsedDuration, reason);
+    const timestamp = Math.floor(Date.now() / 1000 + parsedDuration / 1000);
+    return `${userMention(member.id)} has been muted${reason ? ` for ${inlineCode(reason)}` : ""}. Expiry: ${time(timestamp, TimestampStyles.RelativeTime)}`
 });
