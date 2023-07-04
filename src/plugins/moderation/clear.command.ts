8f0bdb8c4d36422d1392b9d4a2069a1a372c38a5
Meelunae <git@eleuna.me>
2023-07-04 16:16:03 +0200

    First of a long series of fixes for the clear command (now clean)

--- src/plugins/moderation/clear.command.ts
+++ src/plugins/moderation/clear.command.ts
@@ -1,18 +1,28 @@
-import {ChannelType, ChatInputCommandInteraction, PermissionFlagsBits, TextChannel, NewsChannel, ThreadChannel, userMention, channelMention} from "discord.js";
+import {
+    ChannelType,
+    ChatInputCommandInteraction,
+    PermissionFlagsBits,
+    TextChannel,
+    NewsChannel,
+    ThreadChannel,
+    userMention,
+    channelMention,
+    Collection, Message, Snowflake, Channel
+} from "discord.js";
 import {SlashCommandBuilder, SlashCommandScope,} from "../../builders/SlashCommandBuilder";
 import {useChatCommand} from "../../hooks/useChatCommand";
 
 type GuildTextBasedChannel = TextChannel | NewsChannel | ThreadChannel
 
 const builder = new SlashCommandBuilder()
-    .setName("clear")
+    .setName("clean")
     .setDescription("Bulk deletes messages from a given user/channel.")
     .addIntegerOption((option) => 
         option.setName("amount")
         .setDescription("Amount of messages to delete (default 10)")
         .setRequired(false)
         .setMinValue(1)
-        .setMaxValue(100)
+        .setMaxValue(99)
     )
     .addUserOption((option) =>
         option.setName("user").setDescription("User whose messages you want to purge.").setRequired(false)
@@ -24,47 +34,37 @@ const builder = new SlashCommandBuilder()
     .setScope(SlashCommandScope.GLOBAL);
 
     useChatCommand(builder, async (interaction: ChatInputCommandInteraction) => {
+        if (!interaction.guild || !interaction.channel || interaction.channel?.type === ChannelType.DM) throw new Error("This command can only be run in a guild.");
+
         const user = interaction.options.getUser("user", false);
-        const channel = interaction.options.getChannel("channel", false) as TextChannel;
+        const channel = interaction.options.getChannel("channel", false, [ChannelType.GuildText]) ?? interaction.channel;
         const amount = interaction.options.getInteger("amount", false) ?? 10;
         const startDate = searchStartDate();
 
-        if (!interaction.guild) throw new Error("This command can only be run in a guild.");
-        
-        if (!user && !channel && interaction.channel?.type !== ChannelType.DM) {
-            await interaction.channel?.bulkDelete(amount);
-            return `Successfully cleared ${amount} messages.`;
+        let messages = await channel.messages.fetch({ limit: 100, after: startDate});
+        let lastFetchedID = messages.last()?.id;
+
+        if (user) {
+            let userMessages = messages.filter(msg => msg.author.id === user.id);
+            // Edge case: the amount of messages sent from the user in the first batch is higher than the amount we want to purge.
+            if (userMessages.size > amount) userMessages = getFirstMessagesFromCollection(userMessages, amount)
+            while (userMessages.size < amount && lastFetchedID) {
+                if (lastFetchedID) {
+                    messages = await channel.messages.fetch({ limit: 100, after: lastFetchedID});
+                    messages = messages.filter(msg => msg.author.id === user.id);
+                    if (messages.size === 0) break; // collection is empty after filtering so no more messages from that user
+                    userMessages.concat(messages);
+                    lastFetchedID = messages.last()?.id;
+                }
+            }
+            await channel.bulkDelete(userMessages);
+            return `Successfully cleared ${userMessages.size} messages from ${userMention(user.id)}`;
         }
 
-        // If user is not specified we delete all messages from the target channel. Fetching is necessary because of Discord API's 14 days limit.
-        if (!user && channel) {
-            if (!channel.isTextBased()) return `This command can only be ran on text channels`;
-            let messages = await channel.messages.fetch({ limit: amount, after: startDate});
-            await channel.bulkDelete(messages);
-            return `Successfully cleared ${amount} messages in ${channelMention(channel.id)}.`;
-        }
-
-        // For paths that delete messages from a specific user we add an arbitrary number of messages to increase odds we'll actually fetch "amount" messages belonging to said user.
-        if (user && channel) {
-            if (!channel.isTextBased()) return `This command can only be ran on text channels`;
-            let messages = await channel.messages.fetch({ limit: amount+200, after: startDate});
-            messages = messages.filter(msg => msg.author.id === user.id);
-            await channel.bulkDelete(messages);
-            return `Successfully cleared ${messages.size} messages sent by ${userMention(user.id)} in ${channelMention(channel.id)}.`;
-        }
-
-        // If channel is not specified we delete from all channels
-        if (user && !channel) {
-            const channels = interaction.guild?.channels.cache.filter(channel => channel.type === ChannelType.GuildText);
-            channels?.forEach(async channel => {
-                if (!channel.isTextBased()) return;
-                let messages = await channel.messages.fetch({ limit: amount+200, after: startDate });
-                messages = messages.filter(msg => msg.author.id === user.id) // We filter for author if user is specified
-                await (channel as GuildTextBasedChannel).bulkDelete(messages);
-            })
-            return `Successfully cleared ${amount} messages sent by ${userMention(user.id)} in all channels.`;
-        }
-        return `Something went wrong with the clean command.`
+        // For some reason bulkDelete deletes the output of the return statement too, so we have to delete one more message than amount.
+        messages = getFirstMessagesFromCollection(messages, amount+1);
+        await channel.bulkDelete(messages);
+        return `Successfully cleared ${amount} messages in ${channelMention(channel.id)}.`;
     });
     
     const searchStartDate = () : string => {
@@ -73,4 +73,14 @@ const builder = new SlashCommandBuilder()
         // See https://discord.com/developers/docs/reference#snowflake-ids-in-pagination-generating-a-snowflake-id-from-a-timestamp-example for conversion from timestamp to Snowflake.
         const dateSnowflake = (BigInt(startDate.getTime()) - 1420070400000n) << 22n;
         return dateSnowflake.toString();
+    }
+
+    const getFirstMessagesFromCollection = (collection: Collection<Snowflake, Message<true>>, amount: number) : Collection<Snowflake, Message<true>> => {
+        const keys = collection.firstKey(amount);
+        const values = collection.first(amount);
+        const messages = new Collection<Snowflake, Message<true>>();
+        for (let i = 0; i < keys.length; i++) {
+            messages.set(keys[i], values[i]);
+          }
+        return messages;
     }
