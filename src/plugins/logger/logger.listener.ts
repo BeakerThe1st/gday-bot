eb42cad6b69774532846fa83126e6b642fafd15e
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-07-05 19:03:30 +1000

    Fix issues w/ logging

--- src/plugins/logger/logger.listener.ts
+++ src/plugins/logger/logger.listener.ts
@@ -33,20 +33,20 @@ useEvent(Events.MessageDelete, (message: Message | PartialMessage) => {
 
 //Role Logs
 useEvent(Events.GuildMemberUpdate, (oldMember: GuildMember | PartialGuildMember, newMember: GuildMember | PartialGuildMember) => {
-    const [newRoles, oldRoles] = [oldMember, newMember].map(({roles}) => roles.cache);
+    const [newRoles, oldRoles] = [newMember, oldMember].map(({roles}) => roles.cache);
     //addedRoles is all roles we didn't previously have, removedRules is all roles we did previously have
     const addedRoles = newRoles.filter((role, roleId) => !oldRoles.has(roleId));
     const removedRoles = oldRoles.filter((role, roleId) => !newRoles.has(roleId));
     const roleWasAdded = addedRoles.size > 0;
-    const roleOfInterest = addedRoles.size > 0 ? addedRoles.first() : removedRoles.first();
+    const roleOfInterest = roleWasAdded ? addedRoles.first() : removedRoles.first();
     if (!roleOfInterest) {
         //No added role nor removed role, therefore this is not a role update
         return;
     }
     const embed = new EmbedBuilder()
-        .setDescription(`:key: ${newMember} (${newMember.user.username}) was ${roleWasAdded ? "added to" : "removed from"} ${roleOfInterest}`)
+        .setDescription(`:key: ${newMember} (${newMember.user.username}) was ${roleWasAdded ? "added to" : "removed from"} ${roleOfInterest} (${roleOfInterest.name})`)
         .setFooter({text: `Member ID: ${newMember.id}`})
-        .setColor(roleWasAdded ? Colors.DarkRed : Colors.Green)
+        .setColor(roleWasAdded ? Colors.Green : Colors.DarkRed)
         .setTimestamp(Date.now());
     log(LOG_THREADS.ROLE, embed);
 });
