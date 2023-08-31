bc0ed85ca335f2497d95ea786d4ff26d0815ab65
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-08-31 13:56:30 +1000

    Add new bingo items

--- src/plugins/bingo/bingoItems.ts
+++ src/plugins/bingo/bingoItems.ts
@@ -1,6 +1,10 @@
 export const bingoItems = new Map<string, boolean>();
 
-const items = ["free_space"];
+const items =
+    ["free_space", "titanium", "sapphire_crystal", "thicker_phone", "usbc", "thinner_bezel",
+        "mute_switch", "brighter_display", "navy_blue", "3nm_a17", "periscope_lens", "2tb_max",
+        "256gb_base", "wifi_6e", "u2_chip", "esim_expanded", "price_increase"
+    ];
 
 
 items.forEach((item) => {
