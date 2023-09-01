670b73632df0478ae9b09e0bfa298f3600f55bb0
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-09-01 13:22:46 +1000

    Add future bingo items

--- src/plugins/bingo/bingoItems.ts
+++ src/plugins/bingo/bingoItems.ts
@@ -4,7 +4,10 @@ const items =
     ["free_space", "titanium", "sapphire_crystal", "thicker_phone", "usbc", "thinner_bezel",
         "mute_switch", "brighter_display", "navy_blue", "3nm_a17", "periscope_lens", "2tb_max",
         "256gb_base", "wifi_6e", "u2_chip", "esim_expanded", "price_increase", "braided_cables",
-        "usb4", "qi2"
+        "usb4", "qi2", "health_sensor", "watch_pink", "watch_battery", "s9_chip", "ultra_refresh",
+        "new_mac", "apm_2", "ap_price_down", "ap_announced", "icloud_increase", "under_90min",
+        "vision", "good_mornang", "hf1", "appletv_content", "saving_life", "tim_white_shoes",
+        "eu_only"
     ];
 
 
