c211508cb062718ac74a17be0acf5039ac076002
LoganVessell <fullers.scuds_06@icloud.com>
2023-10-15 08:06:55 -0500

    Code be squeaky clean :o

--- src/plugins/misc/commands/jumbo.command.ts
+++ src/plugins/misc/commands/jumbo.command.ts
@@ -10,21 +10,19 @@ const builder = new SlashCommandBuilder()
 
 useChatCommand(builder, async (interaction: ChatInputCommandInteraction) => {
     const emoji = interaction.options.getString('emoji', true).trim();
-    let emojiURL = null
 
-    if (!emoji.startsWith(`<`)) {
+    if (!emoji.startsWith(`<`) && !emoji.endsWith(">")) {
         return "You can't jumbo-ify that, sorry!";
     };
 
-    if (emoji.startsWith("<") && emoji.endsWith(">")) {
-        const id = emoji.match(/\d{15,}/g);
-        if (!id) {
-            return "Invalid emoji format!";
-        }
-        const type = emoji.startsWith("<a:") ? "gif" : "png";
+    const id = emoji.match(/\d{15,}/g);
+    if (!id) {
+        return "Invalid emoji format!";
+    }
 
-        emojiURL = `https://cdn.discordapp.com/emojis/${id}.${type}?quality=lossless`;
-    };
+    const type = emoji.startsWith("<a:") ? "gif" : "png";
+
+    const emojiURL = `https://cdn.discordapp.com/emojis/${id}.${type}?quality=lossless`;
     
     return `${emojiURL}`
 });
