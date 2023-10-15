c363786352c5559d753e601496d9fa7fea2a9ebc
LoganVessell <fullers.scuds_06@icloud.com>
2023-10-15 01:46:16 -0500

    Efficiency and security improvements to /jumbo

--- src/plugins/misc/commands/jumbo.command.ts
+++ src/plugins/misc/commands/jumbo.command.ts
@@ -7,46 +7,30 @@ const builder = new SlashCommandBuilder()
     .setName("jumbo")
     .setDescription("Make G'day blow up an image.")
     .addStringOption(option => option.setName("emoji").setDescription("Emoji to jumbo size.").setRequired(true))
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
-    .setEphemeral(true)
-    .setScope(SlashCommandScope.GLOBAL);
+    .setScope(SlashCommandScope.MAIN_GUILD);
 
 useChatCommand(builder, async (interaction: ChatInputCommandInteraction) => {
-    let emoji = interaction.options.getString('emoji')?.trim();
+    let emoji = interaction.options.getString('emoji', true)?.trim();
 
     if (!emoji || !interaction.channel) {
         return "Invalid request.";
     }
 
-    // Handle custom emojis
-    if (emoji?.startsWith("<") && emoji.endsWith(">")) {
+    if (emoji.startsWith("<") && emoji.endsWith(">")) {
         const match = emoji.match(/\d{15,}/g);
         if (!match) {
             return "Invalid emoji format!";
         }
-
-        const id = match[0];
-
         let type = "png";
-        try {
-            const image = await axios.get(`https://cdn.discordapp.com/emojis/${id}.gif`);
-            if (image) type = "gif";
-        } catch (err) {};
-        emoji = `https://cdn.discordapp.com/emojis/${id}.${type}?quality=lossless`;
+        if (emoji.startsWith("<a:")) {
+            type = "gif"
+        };
+        emoji = `https://cdn.discordapp.com/emojis/${match}.${type}?quality=lossless`;
     }
 
-    // Check for valid image URLs
-    if (!emoji?.startsWith("http")) {
+    if (!emoji?.startsWith("<:" || "<a:")) {
         return "You can't jumbo-ify that, sorry!";
     }
 
-    // Send the jumbofied emoji
-    await interaction.channel.send({
-        content: emoji,
-        allowedMentions: {
-            parse: ["users"],
-        },
-    });
-
-    return `Jumbofied!`;
+    return `${emoji}`
 });
