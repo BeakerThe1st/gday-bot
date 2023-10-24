bc2edaa99116a8859a9d57c5e1165ff1f998ca5e
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-10-25 08:18:05 +1100

    Welcome back bingo!

---
+++ src/plugins/bingo/bingoItems.ts
@@ -0,0 +1,36 @@
+
+export const bingoItems = new Map([
+    // Free Space
+    ["free_space", "Free Space"],
+
+    // iPhone
+    ["price_increase", "USD Price Increase"],
+
+    // Mac
+    ["new_mac", "New Mac"],
+
+    // AirPods
+    ["apm_2", "AirPods Max 2"],
+    ["ap_announced", "New AirPods Announced"],
+
+    // Troll
+    ["icloud_increase", "Base iCloud GB Increase"],
+    ["someone_dances", "Someone Dances"],
+    ["speed", "Superhuman Speed"],
+
+    // Other
+    ["under_90min", "Keynote under 90 minutes"],
+    ["vision", "Vision Pro Mentioned"],
+    ["hf1", "Hair Force One strikes again"],
+    ["appletv_content", "AppleTV+ Content"],
+    ["saving_life", "Story about an Apple product saving a life"],
+    ["tim_white_shoes", "Tim Cook wears white shoes"],
+    ["eu_only", "\"Only in the EU\""],
+    ["rainbow", "Rainbow Cameo"],
+    ["underground_lab", "Underground Lab"],
+    ["carplay", "Carplay Mentioned"],
+    ["meaningless_graph", "Meaningless Graph"],
+    ["third_party_demo", "Boring Third Party Demo"],
+    ["woaaw", "\"Woaaw!\""],
+    ["apple_park_night", "Apple Park at Night"]
+]);
