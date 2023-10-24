e7ff62f4040319e07c927e09069fb20c6c5ccac5
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-10-25 07:39:05 +1100

    Add scary fast apple event

--- src/globals.ts
+++ src/globals.ts
@@ -6,12 +6,12 @@ interface NextEvent {
 }
 
 //Timestamp is in ms
-export const NEXT_EVENT: null | NextEvent = null; /*{
-    name: "Wonderlust",
-    timestamp: 1694538000000,
-    image: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGV4dXgwOTI3OWxvbDUxc3R0cTB1ZTJ3ZzdqamVycjdqbWtkbGQwNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rFojqMvKmk3ZCliupx/giphy.gif",
-    color: "#7cb1d6",
-};*/
+export const NEXT_EVENT: null | NextEvent = {
+    name: "Scary Fast",
+    timestamp: 1698710400000,
+    image: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWVibTN5Y2ZiMGxuMzk4bmwzb2lyZmJjODNsN3dpMWhjcXA2b2FseiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DbEimmh5VeAM4Fu0Xc/giphy.gif",
+    color: "#dbdbdf",
+};
 
 
 //THESE GUILD IDS ARE MIRRORED SlashCommandBuilder, CHANGE THEM THERE TOO
