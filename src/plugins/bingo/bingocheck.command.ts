877264091ee88a214c7b5d80ab5806a0f55f5833
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-10-31 12:12:28 +1100

    Unfinalise bingocheck

--- src/plugins/bingo/bingocheck.command.ts
+++ src/plugins/bingo/bingocheck.command.ts
@@ -17,7 +17,6 @@ const builder = new SlashCommandBuilder()
     .setEphemeral(true)
     .setScope(SlashCommandScope.MAIN_GUILD);
 useChatCommand(builder as SlashCommandBuilder, async (interaction: ChatInputCommandInteraction) => {
-    return "nuh uh";
     /*const id = interaction.options.getString("bingo_id", true);
     if (!Array.from(bingoItems.keys()).includes(id)) {
         return "Not a bingo key!"
