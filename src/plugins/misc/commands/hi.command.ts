9aff0a86fe7aa3419c36374c08accf112236af84
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2024-04-08 18:30:58 +1000

    Make hi global again!

--- src/plugins/misc/commands/hi.command.ts
+++ src/plugins/misc/commands/hi.command.ts
@@ -1,6 +1,6 @@
 import {SlashCommandBuilder, SlashCommandScope} from "../../../builders/SlashCommandBuilder";
 import {useChatCommand} from "../../../hooks/useChatCommand";
 
-const builder = new SlashCommandBuilder().setName("hi").setDescription("Quick way to say g'day, keep it simple.").setScope(SlashCommandScope.MAIN_GUILD);
+const builder = new SlashCommandBuilder().setName("hi").setDescription("Quick way to say g'day, keep it simple.");
 
 useChatCommand(builder, (interaction) => ("hi!"));
