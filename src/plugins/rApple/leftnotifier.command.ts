2d50f16804ba7befa7a7026588d0b128dbc01f2c
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2024-07-05 16:13:42 +1000

    Add notification when the person you are replying to has left

---
+++ src/plugins/rApple/leftnotifier.command.ts
@@ -0,0 +1,37 @@
+import { useClient, useEvent } from "../../hooks";
+import { GUILDS } from "../../globals";
+import { DiscordAPIError, User } from "discord.js";
+
+const userCooldown = new Set<User>();
+
+useEvent("messageCreate", async (message) => {
+    if (message.guildId !== GUILDS.MAIN) {
+        return;
+    }
+
+    const rApple = await useClient().guilds.fetch(GUILDS.MAIN);
+    let reference;
+    try {
+        reference = await message.fetchReference();
+    } catch {
+        return;
+    }
+
+    if (userCooldown.has(reference.author)) {
+        return;
+    }
+
+    try {
+        await rApple.members.fetch(reference.author);
+    } catch (error) {
+        if (error instanceof DiscordAPIError && error.status === 404) {
+            userCooldown.add(reference.author);
+            setTimeout(() => userCooldown.delete(reference.author), 15000);
+            await message.reply(
+                `Heads up mate, the person you're replying to is no longer in the server.`,
+            );
+        } else {
+            throw error;
+        }
+    }
+});
