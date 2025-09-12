706c8a611e92edeb4fc5147ba64ef64978ad1920
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2025-09-12 23:12:41 +1000

    Revert "Change vanity listener to use message created event rather than typin…"
    This reverts commit f4569472608f1431748e64c69227663d33a6509a.

--- src/plugins/vanity/vanityListener.listener.ts
+++ src/plugins/vanity/vanityListener.listener.ts
@@ -2,7 +2,7 @@ import { Typing } from "discord.js";
 import { useEvent } from "../../hooks";
 import { VANITY_EVENT, GUILDS } from "../../globals";
 
-useEvent("messageCreate", async (typing: Typing) => {
+useEvent("typingStart", async (typing: Typing) => {
     if (!VANITY_EVENT) {
         return;
     }
